ef439c9eb7fd3719aa229f3b9eac2ae9f8f8441f
Chung-Ying <ag.cyho@gmail.com>
2024-11-14 21:19:15 +0800

    feat: add first article

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -45,6 +45,8 @@ const config = {
           sidebarPath: './sidebars.js',
         },
         blog: {
+          blogSidebarTitle: 'All posts',
+          blogSidebarCount: 'ALL',
           showReadingTime: true,
           feedOptions: {
             type: ['rss', 'atom'],
